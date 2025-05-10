d03780c93a55d7fde20e20dc1d3050dfb34d97dc
ParisaAg <parisaaghaei17@gmail.com>
2025-05-10 10:37:18 +0330

    first commit

---
+++ posts/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class Post(models.Model):
+    title=models.CharField(max_length=100)
+    urls=models.URLField()
+    authour=models.ForeignKey(User,on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_ad=models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+
+
+class Vote(models.Model):
+    voter=models.ForeignKey(User,on_delete=models.CASCADE)
+    post=models.ForeignKey(Post,on_delete=models.CASCADE)
